e34983b197193815d0ae1238553c61c1497d4e4a
tjddyd55-crypto <tjddyd55@gmail.com>
2026-04-24 15:58:33 +0900

    Expose parser list to renderer

--- preload.js
+++ preload.js
@@ -3,6 +3,7 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("api", {
   crawl: (url) => ipcRenderer.invoke("crawl", url),
   getVersion: () => ipcRenderer.invoke("app-version"),
+  getParsers: () => ipcRenderer.invoke("parser-list"),
   inspect: (payload) => ipcRenderer.invoke("inspect", payload),
   apiFind: (payload) => ipcRenderer.invoke("api-find", payload),
 });
